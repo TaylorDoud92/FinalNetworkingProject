6881a1ad5a5430dcaa229d06fc025ed6d99db674
TaylorDoud92 <taylor.o.doud@gmail.com>
2018-12-03 08:01:09 -0700

    Trying to figure out how to synchronize the sockets
    so they don't conflict with eachother

--- Final Project/client.cpp
+++ Final Project/client.cpp
@@ -3,60 +3,68 @@
 #include <iostream>
 #include <string>
 #include <bitset>
+#include <thread>
+#include <chrono>
 #include "Frame.h"
 
 int main(int argc, int argv[]) {
 	
+	Frame incomingFrame;
+	Frame sendingFrame;
+
+	std::string fileName;
+	std::string incomingLine;
+	std::string outgoingLine;
 	std::string ipaddress;
+
 	std::cout << "Please Enter IP address of server" << "\n";
-//	std::getline(std::cin,ipaddress);
-	std::cout << "We're past getLine";
+	std::getline(std::cin,ipaddress);
+	std::cout << "Enter File to transfer \n";
+	std::getline(std::cin, fileName);
 
 	try {
 
 		// Replace "localhost" with the hostname
 		// that you're running your server.
-		ClientSocket dataChannel("localhost", 30000);
-		ClientSocket ackChannel("localhost", 30001);
+		ClientSocket dataChannel("localhost", 29999);
+		ClientSocket ackChannel("localhost", 30000);
 
-		Frame incomingFrame;
-		Frame sendingFrame;
-		
-		std::string fileName;
-		std::string incomingLine;
-		std::string outgoingLine;
+		dataChannel << fileName;
 
-		std::getline(std::cin, fileName);
+		while(true){
+			dataChannel >> incomingLine;
+			std::cout << incomingLine;
+			std::this_thread::sleep_for(std::chrono::seconds(2));
+		}
 
 		try{
 			dataChannel << fileName;
 		} catch (SocketException& e){
 			std::cout << "Exception was caught:" << e.description() << "\n";
 		}
-		// Usually in real applications, the following
-		// will be put into a loop.
 
-		while(true){
 
-				try {
-					dataChannel >> incomingLine;
-
-					incomingFrame.characters = incomingLine.substr(0,incomingLine.size()-1);
-					incomingFrame.parity = incomingLine.back();
-					incomingFrame.asciiSum();
-
-					if(incomingFrame.compareParity()){
-						ackChannel << "ACK";
-						std::cout << incomingFrame.characters;
-					} else {
-						ackChannel << "NAK";
-					}
-
-				} catch (SocketException&) {
-
-				}
-
-		}
+//		while(true){
+//				try {
+//					dataChannel >> incomingLine;
+//
+//					incomingFrame.characters = incomingLine.substr(0,incomingLine.size()-1);
+//					incomingFrame.parity = incomingLine.back();
+//					incomingFrame.asciiSum();
+//
+//					if(incomingFrame.compareParity()){
+//						ackChannel << "ACK";
+//						std::cout << incomingFrame.characters;
+//					} else {
+//						ackChannel << "NAK";
+//					}
+//
+//					std::cout << "alive";
+//				} catch (SocketException&) {
+//
+//				}
+//
+//		}
 
 	} catch (SocketException& e) {
 		std::cout << "Exception was caught:" << e.description() << "\n";
--- Final Project/server.cpp
+++ Final Project/server.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <thread>
+#include <chrono>
 #include "Frame.h"
 
 using namespace std;
@@ -12,83 +14,89 @@ int main(int argc, int argv[]) {
 	Frame incomingFrame;
 	Frame outgoingFrame;
 	int counter = 0;
-	std::string ack;
+	string ack;
+	string fileName;
 	srand(5);
 
 	cout << "running....\n";
 
 	try {
 		// Create the socket and pass port number
-
-		ServerSocket dataChannel(30000);
-
-		ServerSocket ackChannel(30001);
-		string fileName;
-
-		cout << "Created Server Socket... \n";
-
-		cout << "Reading File... \n";
-
 		while (true) {
-			ServerSocket data_sock;
-			ServerSocket ack_sock;
-			dataChannel.accept(data_sock);			//Start listening for data Socket and ack Socket
-			ackChannel.accept(ack_sock);
 
-			// For multiple threading, you need to create
-			// a new thread here and pass new_sock to it.
-			// The thread will use new_sock to communicate
-			// with the client.
+		ServerSocket dataChannel(29999);
+		ServerSocket ackChannel(30000);
+
+
+		cout << "Created Server Socket... \n" << std::endl;
+		cout << "Reading File... \n" << std::endl;
+
+		ServerSocket data_sock;
+		ServerSocket ack_sock;
+		dataChannel.accept(data_sock);			//Start listening for data Socket and ack Socket
+		ackChannel.accept(ack_sock);
+
+
+			cout << "Connected Socks \n";
 
 			try {
-				data_sock >> fileName;								//Pass the fileName from the client socket to the Server
+				data_sock >> fileName;
+
+
+				data_sock << fileName;
+				cout << "resent FileName";
+//				std::this_thread::sleep_for(std::chrono::seconds(2));
+				data_sock << fileName;
+				cout << "resent FileName";
+//				data_sock >> fileName;								//Pass the fileName from the client socket to the Server
+//				cout << fileName;
 				string incomingStr;									//Variable to contain our incoming line
 				string formattedStr;								//Variable to contain a section of our formatedline
 
-				ifstream inFile(fileName);							//Create input file stream to grab lines from file
-
-				while (getline(inFile, incomingStr)) {							//Start pulling lines from file
-
-					while(incomingStr.length() > 64){							//Loop as long as incoming string is longer then 64 characters
-						outgoingFrame.characters = incomingStr.substr(0,63);	//substring a portion of the string to be sent to the client
-						outgoingFrame.asciiSum();									//Get sum of ascii characters and convert it to binary for parity bit
-
-						if(rand() != 5 || counter != 5){
-							if(outgoingFrame.convertBinary(outgoingFrame.checkSum)%2 == 0){
-								outgoingFrame.parity = 0;
-							} else {
-								outgoingFrame.parity = 1;
-							}
-							data_sock << outgoingFrame.characters + std::to_string(outgoingFrame.parity);
-							counter++;
-						} else {
-							if(outgoingFrame.convertBinary(outgoingFrame.checkSum)%2 == 0){
-								outgoingFrame.parity = 1;
-							} else {
-								outgoingFrame.parity = 0;
-							}
-							counter = 0;
-						}
-						ack_sock >> ack;
-						if(ack == "NAK"){
-							if(outgoingFrame.convertBinary(outgoingFrame.checkSum)%2 == 0){
-								outgoingFrame.parity = 0;
-							} else {
-								outgoingFrame.parity = 1;
-							}
-							data_sock << outgoingFrame.characters + std::to_string(outgoingFrame.parity);
-						}
-					}
-					outgoingFrame.characters = incomingStr.substr(0,incomingStr.length()-1);
-					outgoingFrame.asciiSum();
-					if(outgoingFrame.convertBinary(outgoingFrame.checkSum)%2 == 0){
-						outgoingFrame.parity = 0;
-					} else {
-						outgoingFrame.parity = 1;
-					}
-					data_sock << outgoingFrame.characters + std::to_string(outgoingFrame.parity);
-				}
-				inFile.close();
+//				ifstream inFile(fileName);							//Create input file stream to grab lines from file
+//
+//				while (getline(inFile, incomingStr)) {							//Start pulling lines from file
+//
+//					while(incomingStr.length() > 64){							//Loop as long as incoming string is longer then 64 characters
+//						outgoingFrame.characters = incomingStr.substr(0,63);	//substring a portion of the string to be sent to the client
+//						outgoingFrame.asciiSum();									//Get sum of ascii characters and convert it to binary for parity bit
+//
+//						if(rand() != 5 || counter != 5){
+//							if(outgoingFrame.convertBinary(outgoingFrame.checkSum)%2 == 0){
+//								outgoingFrame.parity = 0;
+//							} else {
+//								outgoingFrame.parity = 1;
+//							}
+//							data_sock << outgoingFrame.characters + std::to_string(outgoingFrame.parity);
+//							counter++;
+//						} else {
+//							if(outgoingFrame.convertBinary(outgoingFrame.checkSum)%2 == 0){
+//								outgoingFrame.parity = 1;
+//							} else {
+//								outgoingFrame.parity = 0;
+//							}
+//							counter = 0;
+//						}
+//						ack_sock >> ack;
+//						if(ack == "NAK"){
+//							if(outgoingFrame.convertBinary(outgoingFrame.checkSum)%2 == 0){
+//								outgoingFrame.parity = 0;
+//							} else {
+//								outgoingFrame.parity = 1;
+//							}
+//							data_sock << outgoingFrame.characters + std::to_string(outgoingFrame.parity);
+//						}
+//					}
+//					outgoingFrame.characters = incomingStr.substr(0,incomingStr.length()-1);
+//					outgoingFrame.asciiSum();
+//					if(outgoingFrame.convertBinary(outgoingFrame.checkSum)%2 == 0){
+//						outgoingFrame.parity = 0;
+//					} else {
+//						outgoingFrame.parity = 1;
+//					}
+//					data_sock << outgoingFrame.characters + std::to_string(outgoingFrame.parity);
+//				}
+//				inFile.close();
 			} catch (SocketException& e) {
 				std::cout << "Exception was caught:" << e.description() << "\nExiting.\n";
 			}
